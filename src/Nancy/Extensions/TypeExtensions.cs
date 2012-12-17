b170154f68ae0b939f0717802795fdbdb7a493dd
Jonathan Channon <jonathan.channon@gmail.com>
2012-12-17 17:19:22 +0000

    Added number and datetime converters and tests

--- src/Nancy/Extensions/TypeExtensions.cs
+++ src/Nancy/Extensions/TypeExtensions.cs
@@ -34,5 +34,40 @@
 
             return source.IsGenericType && source.GetGenericTypeDefinition() == enumerableType;
         }
+
+        /// <summary>
+        /// Determines if a type is numeric.  Nullable numeric types are considered numeric.
+        /// </summary>
+        /// <remarks>
+        /// Boolean is not considered numeric.
+        /// </remarks>
+        public static bool IsNumeric(this Type source)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            switch (Type.GetTypeCode(source))
+            {
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Single:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                    return true;
+                case TypeCode.Object:
+                    if (source.IsGenericType && source.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        return IsNumeric(Nullable.GetUnderlyingType(source));
+                    }
+                    return false;
+            }
+            return false;
+        }
     }
 }
